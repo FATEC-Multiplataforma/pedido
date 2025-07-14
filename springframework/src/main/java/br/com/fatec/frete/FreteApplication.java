15fceb029aefe8d4265a7a082320fa411dfc31b1
MrSpock182 <klebernunes182@gmail.com>
2025-07-14 15:06:27 -0300

    correcao log redis

--- springframework/src/main/java/br/com/fatec/frete/FreteApplication.java
+++ springframework/src/main/java/br/com/fatec/frete/FreteApplication.java
@@ -1,6 +1,8 @@
 package br.com.fatec.frete;
 
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,8 +12,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication
-public class FreteApplication {
+public class FreteApplication implements CommandLineRunner {
+    @Value("${spring.redis.host}")
+    String host;
+    @Value("${spring.redis.port}")
+    String port;
+
     public static void main(String[] args) {
         SpringApplication.run(FreteApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Connecting to Redis at Host: " + host + " Port:" + port);
     }
 }
